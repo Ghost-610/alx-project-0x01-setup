32350f8619149720a9f7ee05b60aad66d68562a7
elorm gershon <alidugershon@gmail.com>
2025-07-18 00:17:27 +0000

    3. Implement Users Card Components

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -41,4 +41,14 @@ const UsersPage: React.FC = () => {
   );
 };
 
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
 export default UsersPage;
